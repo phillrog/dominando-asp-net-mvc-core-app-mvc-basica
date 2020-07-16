3754d54726152b3d90141f6ff5ee7d16845d7ab7
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-07-15 21:16:00 -0300

    dockerizando

--- AppMvcBasica/AppMvcBasica/Startup.cs
+++ AppMvcBasica/AppMvcBasica/Startup.cs
@@ -27,9 +27,11 @@ namespace AppMvcBasica
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddDbContext<ApplicationDbContext>(options =>
-				options.UseSqlServer(
-					Configuration.GetConnectionString("DefaultConnection")));
+			services.AddEntityFrameworkNpgsql()
+			 .AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+			//services.AddDbContext<ApplicationDbContext>(options =>
+			//	options.UseSqlServer(
+			//		Configuration.GetConnectionString("DefaultConnection")));
 			services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
 				.AddEntityFrameworkStores<ApplicationDbContext>();
 			services.AddControllersWithViews();
