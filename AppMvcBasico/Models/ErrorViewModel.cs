95defb81417490ac3220490aaac5e6d44cc58627
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-07-16 10:05:56 -0300

    Reinicio e configuração docker

---
+++ AppMvcBasico/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppMvcBasico.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
